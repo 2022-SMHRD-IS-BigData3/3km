121c7898f3504f5b64faefd47c886a8164a11120
smhrd <smhrd@DESKTOP-O1DPINR>
2023-04-28 10:42:58 +0900

    오류바꿈

--- GitTest/java/km/frontcontroller/FrontController.java
+++ GitTest/java/km/frontcontroller/FrontController.java
@@ -24,55 +24,55 @@ import km.controller.WriteService;
 
 @WebServlet("*.do")
 public class FrontController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
 
-	private HashMap<String, Command> map = null;
+   private HashMap<String, Command> map = null;
 
-	@Override
-	public void init() throws ServletException {
+   @Override
+   public void init() throws ServletException {
 
-		map = new HashMap<>();
-		map.put("Login.do", new LoginService());
-		map.put("Main.do", new MainService());
-		map.put("Join.do", new JoinService());
-		map.put("JoinSuccess.do", new JoinSuccess());
-		map.put("Info.do", new InfoService());
-		map.put("Write.do", new WriteService());
-	}
+      map = new HashMap<>();
+      map.put("Login.do", new LoginService());
+      map.put("Main.do", new MainService());
+      map.put("Join.do", new JoinService());
+      map.put("JoinSuccess.do", new JoinSuccess());
+      map.put("Info.do", new InfoService());
+      map.put("Write.do", new WriteService());
+   }
 
-	
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+   
+   
+   protected void service(HttpServletRequest request, HttpServletResponse response)
+         throws ServletException, IOException {
 
-		String uri = request.getRequestURI();
-		String cp = request.getContextPath();
-		String finaluri = uri.substring(cp.length() + 1);
-		// 한글인코딩
-		request.setCharacterEncoding("utf-8");
+      String uri = request.getRequestURI();
+      System.out.println("애는 uri" + uri);
+      String cp = request.getContextPath();
+      String finaluri = uri.substring(cp.length() + 1);
+      // 한글인코딩
+      request.setCharacterEncoding("utf-8");
 
-		String finalpath = null;
-		Command com = null;
+      String finalpath = null;
+      Command com = null;
 
-		if (finaluri.contains("Go")) {
-			finalpath = finaluri.replaceAll("Go", "").toLowerCase().replaceAll(".do", ".jsp");
-		} else {
-			com = map.get(finaluri);
-			if(com == null) {
-				finalpath = "login.jsp";
-			}else {
-				finalpath = com.execute(request, response);
-			}
-		}
+      if (finaluri.contains("Go")) {
+         finalpath = finaluri.replaceAll("Go", "").toLowerCase().replaceAll(".do", ".jsp");
+      } else {
+         com = map.get(finaluri);
+         if(com == null) {
+            finalpath = "login.jsp";
+         }else {
+            finalpath = com.execute(request, response);
+         }
+      }
 
-		
-		if(finalpath == null) {
-		}else if (finalpath.contains("redirect:/")) {
-			response.sendRedirect(finalpath.substring(10));
-		} else {
-			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/" + finalpath);
-			rd.forward(request, response);
-		}
-		
-	}
+      if(finalpath == null) {
+      }else if (finalpath.contains("redirect:/")) {
+         response.sendRedirect(finalpath.substring(10));
+      } else {
+         RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/" + finalpath);
+         rd.forward(request, response);
+      }
+      
+   }
 }
